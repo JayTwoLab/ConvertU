0f19ab2c1d99d7f35f3623751808a7b5635de01c
j2doll <j2doll@gmail.com>
2019-08-23 17:28:22 +0900

    최초 커밋

---
+++ ConvertU/ConvertU.h
@@ -0,0 +1,32 @@
+
+// ConvertU.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CConvertUApp:
+// See ConvertU.cpp for the implementation of this class
+//
+
+class CConvertUApp : public CWinApp
+{
+public:
+	CConvertUApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CConvertUApp theApp;
---
+++ ConvertU/ConvertUDlg.h
@@ -0,0 +1,34 @@
+
+// ConvertUDlg.h : header file
+//
+
+#pragma once
+
+
+// CConvertUDlg dialog
+class CConvertUDlg : public CDialogEx
+{
+// Construction
+public:
+	CConvertUDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CONVERTU_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
